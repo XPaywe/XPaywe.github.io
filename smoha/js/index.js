0b4bda5a1261da3ed5a60b707ed88107d67dbb69
Islam Rostom <islam.rostom93@gmail.com>
2021-01-27 15:03:00 +0200

    update language switcher

--- smoha/js/index.js
+++ smoha/js/index.js
@@ -2,18 +2,18 @@ import Translator from "./translator.js";
 
 var translator = new Translator();
 
-document.querySelector("form").addEventListener("click", function (evt) {
-  if (evt.target.tagName === "INPUT") {
-    translator.load(evt.target.value);
-  }
-
+document.querySelector("#ar").addEventListener("click", function (evt) {
+  translator.load("ar");
   if (document.documentElement.lang !== this._lang) {
     document.documentElement.lang = this._lang;
   }
+  document.documentElement.dir = "rtl";
+});
 
-  if (evt.target.value === "ar") {
-    document.documentElement.dir = "rtl";
-  } else {
-    document.documentElement.dir = "ltr";
+document.querySelector("#en").addEventListener("click", function (evt) {
+  translator.load("en");
+  if (document.documentElement.lang !== this._lang) {
+    document.documentElement.lang = this._lang;
   }
+  document.documentElement.dir = "ltr";
 });
